95b06e538caa2d2e7f44bbb00e120c2e88aea30e
Nils Stridbeck <nilsjonsson@live.se>
2021-03-24 10:49:20 +0100

    added ourserver

---
+++ ourserver.cc
@@ -0,0 +1,36 @@
+#include "connection.h"
+#include "connectionclosedexception.h"
+#include "server.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+Server init(int argc, char* argv[])
+{
+        if (argc != 2) {
+                cerr << "Usage: myserver port-number" << endl;
+                exit(1);
+        }
+
+        int port = -1;
+        try {
+                port = stoi(argv[1]);
+        } catch (exception& e) {
+                cerr << "Wrong format for port number. " << e.what() << endl;
+                exit(2);
+        }
+
+        Server server(port);
+        if (!server.isReady()) {
+                cerr << "Server initialization error." << endl;
+                exit(3);
+        }
+        return server;
+}
+
+int main(int argc, char* argv[])
+{
+        auto server = init(argc, argv);
+}
